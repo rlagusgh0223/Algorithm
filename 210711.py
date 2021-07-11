76e77cad9ad3ecc50957c2e95c31b93c904e4d5f
rlagusgh0223 <hopanman11@naver.com>
2021-07-11 16:38:52 +0900

    2021/07/11 updated

---
+++ 210711.py
@@ -0,0 +1,19 @@
+"""
+#2557
+print('Hello World!')
+
+#1000
+a,b = input().split()
+print(int (a)+int(b))
+
+#2558
+a = int(input())
+b = int(input())
+print(a+b)
+"""
+#10950
+T = int(input())
+for x in range(0,T):
+    a,b = input().split()
+    print(int (a)+int (b))
+
