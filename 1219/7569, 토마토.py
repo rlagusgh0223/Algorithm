9069635e9f055b27f64cceeb408a33ea51178a88
rlagusgh0223 <hopanman11@naver.com>
2021-12-25 16:31:52 +0900

    2021/12/25

--- 1219/7569, 토마토.py
+++ 1219/7569, 토마토.py
@@ -1,3 +1,4 @@
+#pypy3로 하면 답 나온다
 from collections import deque
 import sys
 
@@ -6,16 +7,13 @@ graph = [[0 for _ in range(N)] for _ in range(H)]
 visited = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
 q = deque()
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-dz = [-1, 1]        #상자 위, 아래로 옮기기 위해(상자 높이가 얼마나 되든 인접한것부터 전파된다)
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
 
 for i in range(H):                          # 토마토 입력
     for j in range(N):
         graph[i][j] = list(map(int, sys.stdin.readline().split()))
-
-for i in range(H):                          # 최초의 토마토 위치 체크(-1도 체크해야 나중에 0으로 보지 않음)
-    for j in range(N):
         for k in range(M):
             if graph[i][j][k] == 1:
                 q.append([i,j,k])
@@ -25,22 +23,14 @@ for i in range(H):                          # 최초의 토마토 위치 체크(
 
 while q:
     z, x, y = q.popleft()
-        
-    for j in range(2):                      # 지금 높이의 위, 아래만 체크
-        nz = z + dz[j]
-        if 0 <= nz < H:
-            if graph[nz][x][y] == 0 and visited[nz][x][y] == 0:
-                visited[nz][x][y] = visited[z][x][y] + 1
-                q.append([nz, x, y])
-
-    for i in range(H):
-        for j in range(4):                  # 지금 높이의 앞, 뒤, 좌, 우만 체크
-            nx = x + dx[j]
-            ny = y + dy[j]
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph[i][nx][ny] == 0 and visited[i][nx][ny] == 0:
-                    visited[i][nx][ny] = visited[i][x][y] + 1
-                    q.append([i, nx, ny])
+    for i in range(6):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        nz = z + dz[i]
+        if 0 <= nx < N and 0 <= ny < M and 0 <= nz < H:
+            if graph[nz][nx][ny] == 0 and visited[nz][nx][ny] == 0:
+                visited[nz][nx][ny] = visited[z][x][y] + 1
+                q.append([nz, nx, ny])
 
 ans = 0
 endFlag = True
