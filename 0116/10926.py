cfba9f4e3cd712ea8b59e1e0601c9cbdd63e8012
rlagusgh0223 <hopanman11@naver.com>
2022-01-26 10:26:46 +0900

    2022/01/26

---
+++ 0116/10926.py
@@ -0,0 +1,10 @@
+print(input()+'??!')
+# 아이디를 여러개 입력 받으면 이런식으로 해서 확인해야 될 것 같은데
+# 인터넷을 보니 그냥 무조건 ??!만 붙이면 되는것 같다
+# import sys
+# word = sys.stdin.readline().rstrip()
+# # 전방 일치. 문자열에 검색 문자가 앞에서부터 일치하는지 판정. 일치하면 True 불일치면 False를 리턴함
+# if word.startswith('joonas'):
+#     print("joonas??!")
+# else:
+#     print(word)
--- 0116/16234, 인구이동.py
+++ 0116/16234, 인구이동.py
@@ -1,4 +1,5 @@
 from collections import deque
+from struct import pack
 import sys
 
 N, L, R = map(int, sys.stdin.readline().split())
@@ -10,40 +11,43 @@ for i in range(N):
     graph[i] = list(map(int, sys.stdin.readline().split()))
 
 flag = True
-ans = 0
+ans = -1
 united = 0  # 연합한 국가인지 분간하고 연합이 여러개일 경우 구분하기 위한 변수
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def open(x, y):     # 인구이동이 가능한 구간인지 확인
-    global united
-    united += 1
-    visited[x][y] = united
+    global flag
+    global visited
+    visited[x][y] = -1
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < N and 0 <= ny < N:
-            if L <= abs(graph[nx][ny] - graph[x][y]) <= R:
-                visited[nx][ny] = united
+            if L <= abs(graph[nx][ny] - graph[x][y]) <= R and visited[nx][ny] == 0:
                 flag = True
+                return True
+    return False
 
 def BFS(x,y):
+    global visited
     sum = graph[x][y]
     cnt = 1
     q = deque()
-    q.append((x,y))
+    q.append([x,y])
     unitedq = deque()
     unitedq.append((x,y))
-    visited[x][y] = -1 * visited[x][y]
+    visited[x][y] = 1
+
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < N and 0 <= ny < N:
-                if visited[nx][ny] == -1 * visited[x][y]:
-                    visited[nx][ny] = -1 * visited[nx][ny]
+                if  L <= abs(graph[nx][ny] - graph[x][y]) <= R and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
                     q.append((nx,ny))
                     sum += graph[nx][ny]
                     cnt += 1
@@ -55,14 +59,11 @@ def BFS(x,y):
 while flag:
     flag = False
     ans += 1
+    visited = [[0 for _ in range(N)] for _ in range(N)]
 
-    for i in range(N):
+    for i in range(N):          # 인구이동이 가능한 구간인지 확인하여 visited에 표시
         for j in range(N):
-            open(i,j)
-    
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] != 0:
-                BFS(i,j)
+            if visited[i][j]==0 and open(i,j):
+                BFS(i, j)
 
 print(ans)
