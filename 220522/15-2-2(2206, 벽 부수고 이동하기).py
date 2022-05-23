829f8f7ce0dd76f7042536b51c7de2abb3d6e071
rlagusgh0223 <hopanman11@naver.com>
2022-05-23 19:32:18 +0900

    2022/05/23

---
+++ 220522/15-2-2(2206, 벽 부수고 이동하기).py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+N, M = map(int, sys.stdin.readline().split())
+map = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
+visit = [[[0, 0] for _ in range(M)] for _ in range(N)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+def BFS():
+    q = deque()
+    q.append((0, 0, 0))
+    while q:
+        x, y, wall = q.popleft()
+        if x == N-1 and y == M-1:
+            return visit[x][y][wall]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and visit[nx][ny][wall] == 0:
+                if map[nx][ny] == 0:
+                    q.append((nx,ny, wall))
+                    visit[nx][ny][wall] = visit[x][y][wall] + 1
+                elif map[nx][ny] == 1 and wall == 0:
+                    q.append((nx, ny, 1))
+                    visit[nx][ny][1] = visit[x][y][0] + 1
+    return -1
+
+visit[0][0][0] = 1
+print(BFS())
