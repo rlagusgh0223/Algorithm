1998053380f1d213c8edf6faff60546cbac4fe8c
rlagusgh0223 <hopanman11@naver.com>
2022-05-24 18:48:57 +0900

    2022/05/24

--- 220522/15-2-3(14502, 연구소).py
+++ 220522/15-2-3(14502, 연구소).py
@@ -1,11 +1,14 @@
-# 내일 한번 더 풀어볼 것
+# 내일 한번 더 반복
 from collections import deque
 import sys
+
 N, M = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 temp = [[0 for _ in range(M)] for _ in range(N)]
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+
 ans = 0
 
 def BFS(x, y):
@@ -16,7 +19,7 @@ def BFS(x, y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<M and temp[nx][ny]==0:
+            if 0<=nx<N and 0<=ny<M and temp[nx][ny] == 0:
                 temp[nx][ny] = 2
                 q.append((nx, ny))
 
@@ -40,7 +43,6 @@ def DFS(depth, x, y):
                     BFS(i, j)
         get_score()
         return
-    
     for i in range(x, N):
         if i == x:
             for j in range(y+1, M):
@@ -52,8 +54,8 @@ def DFS(depth, x, y):
             for j in range(M):
                 if graph[i][j] == 0:
                     graph[i][j] = 1
-                    DFS(depth+1, i, j)
+                    DFS(depth+1, x, y)
                     graph[i][j] = 0
-            
-DFS(0, 0, -1)
+
+DFS(0,0,-1)
 print(ans)
