bad62bd3b5d93a73bbbf10aa28a824679c9498ef
rlagusgh0223 <hopanman11@naver.com>
2022-05-30 21:34:47 +0900

    2022/05/30

--- 220529(이분탐색)/17-2, (1920, 수찾기).py
+++ 220529(이분탐색)/17-2, (1920, 수찾기).py
@@ -1,24 +1,53 @@
+# start<=end로 두는것 잊지 말것
 import sys
 N = int(sys.stdin.readline())
-A = [int(ans) for ans in sys.stdin.readline().split()]
-A.sort()
+A = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
-AA = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
 
-def binarySeach(target):    # 이분탐색 정의. 인자로는 AA의 원소를 받는다
+A.sort()
+
+for i in B:
     start = 0
     end = N-1
+    chk = True
     while start<=end:
         mid = (start+end)//2
-        if A[mid] == target:
+        if i == A[mid]:
             print(1)
-            return
-        elif A[mid] <= target:
+            chk = False
+            break
+        elif i >= A[mid]:
             start = mid+1
         else:
             end = mid-1
-    print(0)
-    return
+    
+    if chk:
+        print(0)
 
-for i in range(M):    # 배열 AA의 원소들을binarySeach 함수를 통해 이분탐색
-    binarySeach(AA[i])
+############################################
+# 모범답안
+# import sys
+# N = int(sys.stdin.readline())
+# A = [int(ans) for ans in sys.stdin.readline().split()]
+# A.sort()
+# M = int(sys.stdin.readline())
+# B = list(map(int, sys.stdin.readline().split()))
+
+# def binarySeach(target):
+#     start = 0
+#     end = N-1
+#     while start<=end:
+#         mid = (start+end)//2
+#         if A[mid] == target:
+#             print(1)
+#             return
+#         elif A[mid] <= target:
+#             start = mid+1
+#         else:
+#             end = mid-1
+#     print(0)
+#     return
+
+# for i in range(M):
+#     binarySeach(B[i])
--- 220529(이분탐색)/17-3, (1654, 랜선 자르기).py
+++ 220529(이분탐색)/17-3, (1654, 랜선 자르기).py
@@ -1,3 +1,4 @@
+# 완전 방향을 잘못잡았다
 import sys
 K, N = map(int, sys.stdin.readline().split())
 lanson = []
@@ -9,14 +10,14 @@ end = max(lanson)
 answer = 0
 
 while start <= end:
-    mid = (start+end)//2    # 랜선의 길이가 가장 긴 값을 기준으로 중간값을 도출한다(랜선의 개수를 최소로 하기 위해?)
-    num = 0    # 랜선의 길이를 mid로 나누었을 때 생성되는 랜선의 개수
+    mid = (start+end)//2
+    num = 0    # 랜선의 길이를 mid로 나누었을때 생성되는 랜선의 개수
     for lan in lanson:
-        num += lan//mid    # 모든 랜선을 중간값으로 자른 몫을 더한다(랜선의 개수)
-    if num >= N:    # 랜선의 개수가 목표값보다 많다면 더 길게 잘라야 하므로
-        start = mid + 1    # start값 바꾼다
-        answer = max(answer, mid)    # answer값을 가장 큰 mid값으로 설정(최대길이를 구해야 하므로)
+        num += lan//mid
+    if num >= N:
+        start = mid+1
+        answer = max(answer, mid)
     else:
-        end = mid - 1
+        end = mid-1
 
 print(answer)
