b1eb900926b957ccae4c365d15cf5cf4dfdb4034
rlagusgh0223 <hopanman11@naver.com>
2022-06-14 20:08:20 +0900

    2022/06/14

--- 220612(문자열)/19-1-3, (9935, 문자열 폭발).py
+++ 220612(문자열)/19-1-3, (9935, 문자열 폭발).py
@@ -2,26 +2,23 @@ import sys
 s = sys.stdin.readline().rstrip()
 bomb = sys.stdin.readline().rstrip()
 left = []
-
 start = 0
 end = len(s)-1
+
 while start<=end:
-  check = True
   left.append(s[start])
-  print(left)
   start += 1
+  chk = True
   if len(left) >= len(bomb):
     for i in range(len(bomb)):
       if bomb[i] != left[len(left) - len(bomb) + i]:
-        print(len(left) - len(bomb) + i)
-        print(left)
-        check = False
+        chk = False
         break
-    if check == True:
+    if chk:
       for i in range(len(bomb)):
         left.pop()
 
-if len(left)==0:
+if len(left) == 0:
   print("FRULA")
 else:
   for i in left:
