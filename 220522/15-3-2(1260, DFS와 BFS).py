dd883116dd5ddc52472f70d77f65734b71975d89
rlagusgh0223 <hopanman11@naver.com>
2022-06-24 09:24:12 +0900

    2022

--- 220522/15-3-2(1260, DFS와 BFS).py
+++ 220522/15-3-2(1260, DFS와 BFS).py
@@ -2,17 +2,17 @@ from collections import deque
 import sys
 
 N, M, V = map(int, sys.stdin.readline().split())
-graph = [[0 for _ in range(N+1)] for _ in range(N+1)]    # 입력 숫자와 맞추기 위해 1개 더 배열을 만든다
+lst = [[0 for _ in range(N+1)] for _ in range(N+1)]
+visit = [0] * (N+1)
 for i in range(M):
     x, y = map(int, sys.stdin.readline().split())
-    graph[x][y] = graph[y][x] = 1
-visit = [0]*(N+1)
+    lst[x][y] = lst[y][x] = 1
 
 def DFS(V):
     visit[V] = 1
     print(V, end=' ')
-    for i in range(1, N+1):
-        if visit[i] == 0 and graph[V][i] == 1:
+    for i in range(N+1):
+        if visit[i] == 0 and lst[V][i] == 1:
             DFS(i)
 
 def BFS(V):
@@ -20,12 +20,12 @@ def BFS(V):
     q.append(V)
     while q:
         V = q.popleft()
-        print(V, end= ' ')
         visit[V] = 0
-        for i in range(1, N+1):
-            if visit[i] == 1 and graph[V][i] == 1:
-                visit[i] = 0
+        print(V, end=' ')
+        for i in range(N+1):
+            if visit[i] == 1 and lst[V][i] == 1:
                 q.append(i)
+                visit[i] = 0
 
 DFS(V)
 print()
