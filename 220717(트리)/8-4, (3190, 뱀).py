23c249f6019ef4e3c86e8e78d9e5e63613a27bfa
rlagusgh0223 <hopanman11@naver.com>
2022-07-21 06:36:15 +0900

    2022/07/21

--- 220717(트리)/8-4, (3190, 뱀).py
+++ 220717(트리)/8-4, (3190, 뱀).py
@@ -8,39 +8,39 @@ def change(d, c):
         d = (d+1)%4
     return d
 
-n = int(sys.stdin.readline())
-board = [[0]*n for _ in range(n)]
+N = int(sys.stdin.readline())
+board = [[0]*N for _ in range(N)]
 
-k = int(sys.stdin.readline())
-for _ in range(k):
+K = int(sys.stdin.readline())
+for _ in range(K):
     x, y = map(int, sys.stdin.readline().split())
     board[x-1][y-1] = 1
 
-l = int(sys.stdin.readline())
-times = {}
-for _ in range(l):
+L = int(sys.stdin.readline())
+Time = {}
+for _ in range(L):
     x, c = sys.stdin.readline().split()
-    times[int(x)] = c
+    Time[int(x)] = c
 
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 
 direction = 1
 time = 1
-y, x = 0, 0
-snake = deque([[y, x]])
-board[y][x] = 2
+Y, X = 0, 0
+snake = deque([[Y, X]])
+board[Y][X] = 2
 
 while True:
-    y, x = y+dy[direction], x+dx[direction]
-    if 0<=y<n and 0<=x<n and board[y][x]!=2:
-        if board[y][x] != 1:
+    Y, X = Y+dy[direction], X+dx[direction]
+    if 0<=Y<N and 0<=X<N and board[Y][X] != 2:
+        if board[Y][X] != 1:
             delY, delX = snake.popleft()
             board[delY][delX] = 0
-        snake.append([y, x])
-        board[y][x] = 2
-        if time in times.keys():
-            direction = change(direction, times[time])
+        snake.append([Y, X])
+        board[Y][X] = 2
+        if time in Time.keys():
+            direction = change(direction, Time[time])
         time += 1
     else:
         break
--- 220717(트리)/9-4, (1991, 트리 순회).py
+++ 220717(트리)/9-4, (1991, 트리 순회).py
@@ -1,11 +1,9 @@
-import sys
-
 def preorder(now):
     if now != '.':
         print(now, end='')
         preorder(tree[now][0])
         preorder(tree[now][1])
-
+    
 def inorder(now):
     if now != '.':
         inorder(tree[now][0])
@@ -17,12 +15,12 @@ def postorder(now):
         postorder(tree[now][0])
         postorder(tree[now][1])
         print(now, end='')
-        
-n = int(sys.stdin.readline())
+
+N = int(input())
 tree = {}
-for _ in range(n):
-    middle, left, right = sys.stdin.readline().split()
-    tree[middle] = [left, right]
+for _ in range(N):
+    now, left, right = input().split()
+    tree[now] = [left, right]
 
 preorder('A')
 print()
