9981268739994ed03c933d7ad701ca31104688f1
rlagusgh0223 <hopanman11@naver.com>
2022-08-08 20:30:27 +0900

    2022/08/08

--- 220807(재귀와 분할정복)/13-1-2, (11729, 하노이 탑 이동 순서).py
+++ 220807(재귀와 분할정복)/13-1-2, (11729, 하노이 탑 이동 순서).py
@@ -1,12 +1,13 @@
 import sys
-def hanoi(N, start, middle, end):
+
+def hanoi(N, start, to, end):
     if N==1:
         print(start, end)
     else:
-        hanoi(N-1, start, end, middle)
+        hanoi(N-1, start, end, to)
         print(start, end)
-        hanoi(N-1, middle, start, end)
+        hanoi(N-1, to, start, end)
 
-N = int(sys.stdin.readline())
-print(2**N-1)
-hanoi(N, 1, 2, 3)
+K = int(sys.stdin.readline())
+print(2**K-1)
+hanoi(K, 1, 2, 3)
--- 220807(재귀와 분할정복)/13-2-1, (2630, 색종이 만들기).py
+++ 220807(재귀와 분할정복)/13-2-1, (2630, 색종이 만들기).py
@@ -1,15 +1,15 @@
 import sys
 
-def divide(x, y, n):
+def divide(x, y, N):
     global white, blue
     samecolor = paper[x][y]
-    for i in range(x, x+n):
-        for j in range(y, y+n):
+    for i in range(x, x+N):
+        for j in range(y, y+N):
             if samecolor != paper[i][j]:
-                divide(x, y, n//2)
-                divide(x, y+n//2, n//2)
-                divide(x+n//2, y, n//2)
-                divide(x+n//2, y+n//2, n//2)
+                divide(x, y, N//2)
+                divide(x, y+N//2, N//2)
+                divide(x+N//2, y, N//2)
+                divide(x+N//2, y+N//2, N//2)
                 return
     if samecolor == 0:
         white += 1
@@ -18,11 +18,10 @@ def divide(x, y, n):
         blue += 1
         return
 
-
 N = int(sys.stdin.readline())
-paper = [[int(x) for x in sys.stdin.readline().split()] for _ in range(N)]
-white = 0
-blue = 0
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+white = blue = 0
 divide(0, 0, N)
 print(white)
 print(blue)
