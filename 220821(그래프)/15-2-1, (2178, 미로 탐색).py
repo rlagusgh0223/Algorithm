c9bcc204f88b8bff97cf1a80d285568e58eb6697
rlagusgh0223 <hopanman11@naver.com>
2022-08-22 18:57:17 +0900

    2022/08/22

--- 220821(그래프)/15-2-1, (2178, 미로 탐색).py
+++ 220821(그래프)/15-2-1, (2178, 미로 탐색).py
@@ -1,19 +1,19 @@
 from collections import deque
-import sys
-N, M = map(int, sys.stdin.readline().split())
-maze = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
+
+N, M = map(int, input().split())
+maze = [list(map(int, input())) for _ in range(N)]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 q = deque()
-q.append((0, 0))
-maze[0][0] = 1
+q.append([0, 0])
 while q:
     x, y = q.popleft()
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0<=nx<N and 0<=ny<M and maze[nx][ny] == 1:
-            maze[nx][ny] = maze[x][y] + 1
-            q.append((nx, ny))
+        if 0<=nx<N and 0<=ny<M and maze[nx][ny]==1:
+            q.append([nx, ny])
+            maze[nx][ny] += maze[x][y]  # maze[nx][ny]는 1이니까 maze[x][y]+1과 같은 결과가 나온다
 
 print(maze[N-1][M-1])
--- 220821(그래프)/15-2-2, (2206, 벽 부수고 이동하기).py
+++ 220821(그래프)/15-2-2, (2206, 벽 부수고 이동하기).py
@@ -1,29 +1,31 @@
 from collections import deque
-import sys
 
-def BFS():
-    visit = [[[0]*2 for _ in range(M)] for _ in range(N)]
-    visit[0][0][0] = 1
+def BFS(x, y, wall):
+    global answer
+    q = deque()
+    q.append([x, y, wall])
     while q:
         x, y, wall = q.popleft()
-        if x==N-1 and y==M-1:
-            return visit[x][y][wall]
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0<=nx<N and 0<=ny<M and visit[nx][ny][wall]==0:
-                if Map[nx][ny]==0:
-                    visit[nx][ny][wall] = visit[x][y][wall] + 1
+                if maze[nx][ny] == 0:
+                    visit[nx][ny][wall] = visit[nx][ny][wall]+1
                     q.append([nx, ny, wall])
-                if wall == 0 and Map[nx][ny] == 1:
-                    visit[nx][ny][1] = visit[x][y][0] + 1
+                if wall == 0 and maze[nx][ny] == 1:
+                    visit[nx][ny][1] = visit[x][y][0]+1
                     q.append([nx, ny, 1])
     return -1                    
 
-N, M = map(int, sys.stdin.readline().split())
-Map = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
-q = deque()
-q.append([0, 0, 0])
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
-print(BFS())
+
+
+N, M = map(int, input().split())
+maze = [list(map(int, input())) for _ in range(N)]
+visit = [[[0]*2 for _ in range(M)] for _ in range(N)]
+visit[0][0][0] = 1
+answer = 0
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+BFS(0, 0, 0)
+print(answer)
