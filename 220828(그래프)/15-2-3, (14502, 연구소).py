85d10173a983c4b3f0e0bab9dd1ad3d637e0eb31
rlagusgh0223 <hopanman11@naver.com>
2022-08-28 09:45:51 +0900

    2022/08/28

---
+++ 220828(그래프)/15-2-3, (14502, 연구소).py
@@ -0,0 +1,57 @@
+from collections import deque
+import sys
+
+def BFS(x, y):
+    q = deque()
+    q.append([x, y])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<N and 0<=ny<M and temp[nx][ny]==0:
+                temp[nx][ny] = 2
+                q.append([nx, ny])
+        
+def get_score():
+    global answer
+    score = 0
+    for i in range(N):
+        for j in range(M):
+            if temp[i][j] == 0:
+                score += 1
+    answer = max(answer, score)
+
+def DFS(depth, x, y):
+    if depth==3:
+        for i in range(N):
+            for j in range(M):
+                temp[i][j] = graph[i][j]
+        for i in range(N):
+            for j in range(M):
+                if temp[i][j] == 2:
+                    BFS(i, j)
+        get_score()
+        return
+    for i in range(x, N):
+        if i == x:
+            for j in range(y+1, M):
+                if graph[i][j] == 0:
+                    graph[i][j] = 1
+                    DFS(depth+1, i, j)
+                    graph[i][j] = 0
+        else:
+            for j in range(M):
+                if graph[i][j] == 0:
+                    graph[i][j] = 1
+                    DFS(depth+1, i, j)
+                    graph[i][j] = 0
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [[int(x) for x in sys.stdin.readline().split()] for _ in range(N)]
+temp = [[0 for _ in range(M)] for _ in range(N)]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+answer = 0
+DFS(0, 0, -1)
+print(answer)
---
+++ 220828(그래프)/15-3-2, (1260, DFS와 BFS).py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+
+def DFS(V):
+    visit[V] = 1
+    print(V, end=' ')
+    for i in range(1, N+1):
+        if visit[i]==0 and graph[V][i]==1:
+            DFS(i)
+
+def BFS(V):
+    visit[V] = 0
+    q = deque()
+    q.append(V)
+    while q:
+        V = q.popleft()
+        print(V, end=' ')
+        for i in range(1, N+1):
+            if visit[i]==1 and graph[V][i]==1:
+                visit[i]=0
+                q.append(i)
+
+N, M, V = map(int, sys.stdin.readline().split())
+graph = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for i in range(M):
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x][y] = graph[y][x] = 1
+visit = [0] * (N+1)
+DFS(V)
+print()
+BFS(V)
