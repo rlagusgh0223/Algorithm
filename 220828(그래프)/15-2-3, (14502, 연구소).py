f1b4e5e2c1d6ebadaeb311f48dfb5cb786778772
rlagusgh0223 <hopanman11@naver.com>
2022-08-31 07:05:52 +0900

    2022/08/31

--- 220828(그래프)/15-2-3, (14502, 연구소).py
+++ 220828(그래프)/15-2-3, (14502, 연구소).py
@@ -1,5 +1,4 @@
 from collections import deque
-import sys
 
 def BFS(x, y):
     q = deque()
@@ -9,9 +8,8 @@ def BFS(x, y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<M and temp[nx][ny]==0:
-                temp[nx][ny] = 2
-                q.append([nx, ny])
+            if 0<=nx<N and 0<=ny<M and temp[x][y]==0:
+                temp[i][j] = 2
 
 def get_score():
     global answer
@@ -23,7 +21,7 @@ def get_score():
     answer = max(score, answer)
 
 def DFS(depth, x, y):
-    if depth==3:
+    if depth == 3:
         for i in range(N):
             for j in range(M):
                 temp[i][j] = Map[i][j]
@@ -38,17 +36,17 @@ def DFS(depth, x, y):
             for j in range(y+1, M):
                 if Map[i][j] == 0:
                     Map[i][j] = 1
-                    DFS(depth+1, i, j)
+                    DFS(depth+1, x, y)
                     Map[i][j] = 0
         else:
             for j in range(M):
                 if Map[i][j] == 0:
                     Map[i][j] = 1
-                    DFS(depth+1, i, j)
+                    DFS(depth+1, x, y)
                     Map[i][j] = 0
 
-N, M = map(int, sys.stdin.readline().split())
-Map = [[int(x) for x in sys.stdin.readline().split()] for _ in range(N)]
+N, M = map(int, input().split())
+Map = [list(map(int, input().split())) for _ in range(N)]
 temp = [[0 for _ in range(M)] for _ in range(N)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
--- 220828(그래프)/15-3-2, (1260, DFS와 BFS).py
+++ 220828(그래프)/15-3-2, (1260, DFS와 BFS).py
@@ -1,5 +1,4 @@
 from collections import deque
-import sys
 
 def DFS(V):
     visit[V] = 1
@@ -7,7 +6,7 @@ def DFS(V):
     for i in range(1, N+1):
         if visit[i]==0 and graph[V][i]==1:
             DFS(i)
-
+    
 def BFS(V):
     visit[V] = 0
     q = deque()
@@ -17,17 +16,15 @@ def BFS(V):
         print(V, end=' ')
         for i in range(1, N+1):
             if visit[i]==1 and graph[V][i]==1:
+                visit[i] = 0
                 q.append(i)
-                visit[i]=0
 
-N, M, V = map(int, sys.stdin.readline().split())
+N, M, V = map(int, input().split())
 graph = [[0 for _ in range(N+1)] for _ in range(N+1)]
+visit = [0] * (N+1)
 for i in range(M):
-    x, y = map(int, sys.stdin.readline().split())
+    x, y = map(int, input().split())
     graph[x][y] = graph[y][x] = 1
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-visit = [0]*(N+1)
 DFS(V)
 print()
 BFS(V)
