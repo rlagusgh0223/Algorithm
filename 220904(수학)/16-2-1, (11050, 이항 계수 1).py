4e31c914809889d9f919aa74db3fb8777e16d9b7
rlagusgh0223 <hopanman11@naver.com>
2022-09-07 07:01:33 +0900

    2022/09/07

--- 220904(수학)/16-2-1, (11050, 이항 계수 1).py
+++ 220904(수학)/16-2-1, (11050, 이항 계수 1).py
@@ -1,15 +1,10 @@
-N, K = map(int, input().split())
-
-Nf = 1
+import sys
+N, K = map(int, sys.stdin.readline().split())
+Nf = Kf= NKf = 1
 for i in range(1, N+1):
     Nf *= i
-
-Kf = 1
 for i in range(1, K+1):
     Kf *= i
-
-NKf = 1
 for i in range(1, N-K+1):
     NKf *= i
-
-print(Nf // (Kf * NKf))
+print(Nf//(Kf * NKf)) 
