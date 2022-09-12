9dc0e7e13f7d7d581c72c2f9ae53f8dc59f42a7b
rlagusgh0223 <hopanman11@naver.com>
2022-09-12 09:06:05 +0900

    2022/09/12

--- 220911(이분탐색)/17-2, (1920, 수 찾기).py
+++ 220911(이분탐색)/17-2, (1920, 수 찾기).py
@@ -1,23 +1,22 @@
-n = int(input())
-a = list(map(int, input().split()))
-a.sort()  # 이분탐색으로 이용하기 위해 a를 오름차순으로 정렬
+import sys
 
-def binarySeach(target):  # 이분탐색. 인자로는 b의 원소
+def search(now):
     start = 0
-    end = n - 1
+    end = N-1
     while start<=end:
-        mid = (start+end) // 2
-        if a[mid] == target:  # a[mid]가 찾고자 하는 b의 원소와 같다면 1 출력하고 함수 종료
-            print(1)
-            return
-        elif a[mid]<=target:  # a[mid]가 찾고자 하는 b의 원소보다 작다면 start 재지정
+        mid = (start+end)//2
+        if A[mid] == now:
+            return 1
+        elif A[mid] <= now:
             start = mid + 1
-        else:  # a[mid]가 찾고자 하는 b의 원소보다 크다면 end값 재지정
-            end = mid - 1
-    print(0)  # 못 찾은 경우 0을 출력하고 함수 종료
-    return
+        else:
+            end = mid-1
+    return 0
 
-m = int(input())
-b = list(map(int, input().split()))
-for i in range(m):
-    binarySeach(b[i])
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+A.sort()
+M = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+for i in num:
+    print(search(i))
--- 220911(이분탐색)/17-4, (3020, 개똥벌레).py
+++ 220911(이분탐색)/17-4, (3020, 개똥벌레).py
@@ -1,48 +1,47 @@
 import sys
 
-def upper(s, e, d, L):  # 숫자 초과값이 처음으로 나오는 인덱스 반환
-    while e-s > 0:
-        m = (s+e) // 2
-        if L[m] <= d:
-            s = m+1
+def upper(start, end, d, L):
+    while start>end:
+        mid = (start+end) // 2
+        if L[mid] <= d:
+            start = mid+1
         else:
-            e = m
-    return e
+            end = mid
+    return end
 
-def lower(s, e, d, L):  # 숫자 이상의 값이 처음으로 나오는 인덱스 반환
-    while e-s > 0:
-        m = (s+e) // 2
-        if L[m] < d:
-            s = m+1
+def lower(start, end, d, L):
+    while end>start:
+        mid = (start+end) // 2
+        if L[mid] < d:
+            start = mid + 1
         else:
-            e = m
-    return e
+            end = mid
+    return end
 
-up = []   # 장애물을 위->아래 상태로 저장하기 위한 배열
-down = []  # 장애물을 아래->위 상태로 저장하기 위한 배열
-result = [0] * 500001  # 후에 최솟값이 몇 개 있는지 파악하기 위해 쓰인다
-n , h = map(int, sys.stdin.readline().split())
-for i in range(n):
-    obstacle = int(sys.stdin.readline())
-    if i%2 == 1:  # 홀수는 up에, 짝수는 down에 넣는다
-        up.append(obstacle)
+up = []
+down = []
+result = [0] * 500001
+N, H = map(int, sys.stdin.readline().split())
+for i in range(N):
+    num = int(sys.stdin.readline())
+    if i%2 == 1:
+        up.append(num)
     else:
-        down.append(obstacle)
+        down.append(num)
 
 up.sort()
 down.sort()
+answer = 0
+mx = 500000
 
-answer = 0  # 최솟값의 개수
-mx = 2147483647  # 최솟값을 찾기 위한 값 초기화
-
-for i in range(1, h+1):  # 높이 1부터 h까지 반복
-    idxd = lower(0, len(down), i, down) # 아래->위 장애물을 처음 만나는 위치를 찾는다
-    idxu = lower(0, len(up), h-i+1, up)  # 위->아래 장애물을 처음 만나는 위치를 찾는다
-    result[i] = n//2-idxd + n//2-idxu
+for i in range(1, H+1):
+    idxd = lower(0, len(down), i, down)
+    idxu = lower(0, len(up), H-i+1, up)
+    result[i] = N//2-idxd + N//2-idxu
     mx = min(mx, result[i])
 
-for i in range(1, h+1):
-    if result[i] == mx:  # result배열에서 최솟값이 있다면 개수를 세어준다
+for i in range(1, H+1):
+    if result[i] == mx:
         answer += 1
 
-print(mx, answer)  # 최솟값과 최소값의 개수 출력
+print(mx, answer)
