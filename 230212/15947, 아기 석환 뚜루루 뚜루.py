b7c17f49ccf14f90bb3b7cb76270c03afa68cc49
rlagusgh0223 <hopanman11@naver.com>
2023-02-14 11:12:58 +0900

    2023/02/14

---
+++ 230212/15947, 아기 석환 뚜루루 뚜루.py
@@ -0,0 +1,12 @@
+lst = ['baby', 'sukhwan', 'tururu', 'turu',
+'very', 'cute', 'tururu', 'turu',
+'in', 'bed', 'tururu', 'turu',
+'baby', 'sukhwan']
+N = int(input()) - 1
+k = N//14
+if N%14 in [3, 7, 11]:
+    print(f"tu+ru*{k+1}" if k>=4 else "turu"+"ru"*k)
+elif N%14 in [2, 6, 10]:
+    print(f"tu+ru*{k+2}" if k>=3 else "tururu"+"ru"*k)
+else:
+    print(lst[N%14])
