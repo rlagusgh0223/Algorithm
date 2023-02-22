b23e75cfdaf1d2fcfbd87115dbbdea70d18476da
rlagusgh0223 <hopanman11@naver.com>
2023-02-22 10:52:22 +0900

    2023/02/22

--- 230219/3060, 욕심쟁이 돼지.py
+++ 230219/3060, 욕심쟁이 돼지.py
@@ -13,3 +13,23 @@ for _ in range(T):
             break
         ck *= 4
         p += 1
+
+
+
+# import sys
+# T = int(sys.stdin.readline())
+# for _ in range(T):
+#     p = 1
+#     N = int(sys.stdin.readline())
+#     lst = list(map(int, sys.stdin.readline().split()))
+#     ck = [0] * 6
+#     while True:
+#         if sum(lst) > N:
+#             print(p)
+#             break
+#         ck[0] = lst[5] + lst[1] + lst[3]
+#         ck[5] = lst[4] + lst[0] + lst[2]
+#         for i in range(1, 5):
+#             ck[i] = lst[i-1] + lst[i+1] + lst[(i+3)%6]
+#         lst = [lst[i]+ck[i] for i in range(6)]
+#         p += 1
