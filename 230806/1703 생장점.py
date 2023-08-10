890eb70bc6462e29aa4c1147d559e74d3f4ef962
rlagusgh0223 <hopanman11@naver.com>
2023-08-11 07:41:19 +0900

    2023/08/11

--- 230806/1703 생장점.py
+++ 230806/1703 생장점.py
@@ -5,8 +5,8 @@ while True:
         break
     n = 1
     for i in range(lst[0]):
-        # lst[0]은 몇 년동안 인지를 나타내므로 그 다음인
-        # 1, 2 / 3, 4 ... 이런식으로 계산한다
+        # lst[0]은 몇 살 인지를(몇 년 동안 자라고 잘라냈는지) 나타내므로 그 다음인
+        # lst[1], lst[2] / lst[3], lst[4]s ... 이런식으로 계산한다
         # splitting facotr를 곱한 만큼 전년도 보다 가지가 늘어난다
         sf = lst[2*i + 1]
         # cut 만큼 가지를 잘라준다
