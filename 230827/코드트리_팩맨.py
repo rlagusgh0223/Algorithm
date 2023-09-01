adfb2c53422768450785f3ee715adb6f80e6c0eb
rlagusgh0223 <hopanman11@naver.com>
2023-09-01 15:56:57 +0900

    factman_2

--- 230827/코드트리_팩맨.py
+++ 230827/코드트리_팩맨.py
@@ -2,15 +2,13 @@ from collections import deque
 import sys
 m, t = map(int, sys.stdin.readline().split())
 r, c = map(int, sys.stdin.readline().split())
-ground = [[0]*4 for _ in range(4)]
 q = deque()
 egg = [[0]*4 for _ in range(4)]
-ground[r-1][c-1] = -1
+egg[r-1][c-1] = -1
 dx = [0, -1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 0, -1, -1, -1, 0, 1, 1, 1]
 for _ in range(m):
     r, c, d = map(int, sys.stdin.readline().split())
-    ground[r-1][c-1] = d
     q.append((r-1, c-1, d))
 while q:
     r, c, d = q.popleft()
@@ -19,17 +17,18 @@ while q:
     egg[r][c] = d
 
     # 몬스터 이동
-    if ground[r][c] > 0:
-        nxt = 0
-        for k in range(8):
-            I = (ground[r][c]+dx[nxt]) % 8
-            J = (ground[r][c]+dy[nxt]) % 8
-            nxt += 1
-            if 0<=I<4 and 0<=J<4 and ground[I][J]==0:
-                ground[I][J], ground[r][c] = ground[r][c], ground[I][J]
-                break
-    for k in range(4):
-        print(ground[k])
-    print()
+    nxt = 0
+    for k in range(8):
+        I = (d+dx[nxt]) % 8
+        J = (d+dy[nxt]) % 8
+        nxt += 1
+        if 0<=I<4 and 0<=J<4 and egg[I][J] != -1:
+            egg[I][J], egg[r][c] = egg[r][c], egg[I][J]
+            print(I, J)
+            # q.append((I, J, egg[I][J]))
+            break
+    # for k in range(4):
+    #     print(egg[k])
+    # print()
 for i in range(4):
     print(egg[i])
