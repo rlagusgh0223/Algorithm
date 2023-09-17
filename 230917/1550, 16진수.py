2f72b46d687d746471a9022401389083375a7e05
rlagusgh0223 <hopanman11@naver.com>
2023-09-17 10:49:02 +0900

    2023/09/17

---
+++ 230917/1550, 16진수.py
@@ -0,0 +1,10 @@
+# import sys
+# number = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8,
+#           '9':9, 'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15}
+# ans = sys.stdin.readline().rstrip()
+# answer = 0
+# for now in ans:
+#     answer = answer*16 + number[now]
+# print(answer)
+
+print(int(input(), 16))  # 위의것보다 이 한줄이면 끝난다
