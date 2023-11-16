5862965f9f5cf14786895878a75a33b9f03c9657
rlagusgh0223 <hopanman11@naver.com>
2023-11-16 11:03:33 +0900

    스택/큐

--- 231112/프로그래머스, 다리를 지나는 트럭.py
+++ 231112/프로그래머스, 다리를 지나는 트럭.py
@@ -3,16 +3,20 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    bridge = deque([0] * bridge_length)
-    truck_weights = deque(truck_weights)
-    while truck_weights:
+    b = deque([0] * bridge_length)  # 다리 위에 있는 트럭
+    t = deque(truck_weights)  # 지나가야 되는 트럭
+    w = 0  # 다리 위의 트럭 무게(sum(b)로 하면 시간초과 나므로 따로 계산을 해줘야 된다)
+    while t:
         answer += 1
-        bridge.popleft()
-        if sum(bridge) + truck_weights[0] <= weight:
-            bridge.append(truck_weights.popleft())
+        w -= b.popleft()
+        if w+t[0] <= weight:
+            w += t[0]
+            b.append(t.popleft())
         else:
-            bridge.append(0)
-    answer += len(bridge)
+            b.append(0)
+    # 위의 반복문으로는 트럭이 다리에 올라오는 것 까지만 계산되므로,
+    # 올라온 후 지나가는 시간도 더해줘야 한다
+    answer += len(b)
     return answer
 
 
