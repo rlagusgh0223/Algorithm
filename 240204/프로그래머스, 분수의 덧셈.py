ab4da841b571752047a0a78e788173a45c7d8fc7
rlagusgh0223 <hopanman11@naver.com>
2024-02-10 22:40:12 +0900

    2024/02/10

--- 240204/프로그래머스, 분수의 덧셈.py
+++ 240204/프로그래머스, 분수의 덧셈.py
@@ -4,9 +4,9 @@ def gcd(n1, n2):
     return n2
 
 def solution(numer1, denom1, numer2, denom2):
-    n = numer1*denom2 + numer2*denom1  # 분모
-    d = denom1*denom2  # 분자
-    g = gcd(n, d)  # 분모와 분자 간의 최대공약수
+    n = numer1*denom2 + numer2*denom1  # 분자
+    d = denom1*denom2  # 분모
+    g = gcd(n, d)  # 분자와 분모 간의 최대공약수
     return [n//g, d//g]
 
 import sys
