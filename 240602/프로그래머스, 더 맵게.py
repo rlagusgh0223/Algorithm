d06dfdc4dcec10457ea66e92815ba39e5c4eedc8
rlagusgh0223 <hopanman11@naver.com>
2024-06-06 10:33:21 +0900

    2024/06/06

---
+++ 240602/프로그래머스, 더 맵게.py
@@ -0,0 +1,21 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)  # scoville을 heap으로 변환
+    x = heapq.heappop(scoville)  # scovile에서 가장 작은 값 빼서 x에 입력
+    while x < K:
+        y = heapq.heappop(scoville)  # scoville에서 두번째로 작은 값 빼서 입력
+        heapq.heappush(scoville, x+(y*2))
+        answer += 1
+        x = heapq.heappop(scoville)  # 연산 후 scoville에서 가장 작은 값 빼서 x에 입력
+        if len(scoville)==0 and x<K:  # 더 이상 섞을 음식이 없는데 K보다 작을 경우 값이 나올 수 없으므로 -1 리턴
+            return -1
+    return answer
+
+import sys
+
+scoville = list(map(int, sys.stdin.readline().split(",")))
+K = int(sys.stdin.readline())
+print(solution(scoville, K))
