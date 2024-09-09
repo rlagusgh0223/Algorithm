257d1dbc8499d87dc75b139b493e2686ae72414e
rlagusgh0223 <hopanman11@naver.com>
2024-09-09 21:12:48 +0900

    2024/09/09

---
+++ 240908/프로그래머스, 조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    for i in range(len(arr)):
+        if arr[i]>=50 and arr[i]%2==0:
+            arr[i] //= 2
+        elif arr[i]<50 and arr[i]%2==1:
+            arr[i] *= 2
+    return arr
+
+import sys
+
+print(solution(list(map(int, sys.stdin.readline().split(", ")))))
