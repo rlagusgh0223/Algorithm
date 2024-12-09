856b917092340c0b082aa556e2e9325bd08db56d
rlagusgh0223 <hopanman11@naver.com>
2024-12-09 21:32:30 +0900

    2024/12/09

---
+++ 241208/프로그래머스, 최소직사각형.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    w, h = [], []
+    for size in sizes:
+        w.append(max(size))
+        h.append(min(size))
+    return max(w) * max(h)
+import sys
+
+sizes = list(sys.stdin.readline().strip().split("], ["))
+sizes = [list(map(int, s.split(", "))) for s in sizes]
+print(solution(sizes))
