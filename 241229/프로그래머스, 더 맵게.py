f5da809fe4016b0d164c5c3011b48ef30527bf29
rlagusgh0223 <hopanman11@naver.com>
2024-12-29 09:08:24 +0900

    2024/12/29

---
+++ 241229/프로그래머스, 더 맵게.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while len(scoville) > 1:
+        x = heapq.heappop(scoville)
+        if x >= K:
+            return answer
+        y = heapq.heappop(scoville)
+        answer += 1
+        heapq.heappush(scoville, x + 2*y)
+    return -1 if scoville[0]<K else answer
+
+import sys
+
+s = list(map(int, sys.stdin.readline().split(", ")))
+k = int(sys.stdin.readline())
+print(solution(s, k))
