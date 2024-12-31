56a6cc5c7b5a4a489723e71ced5a75913d0e06ba
rlagusgh0223 <hopanman11@naver.com>
2024-12-31 18:53:25 +0900

    2024/12/31

---
+++ 241229/프로그래머스, 닉네임 규칙.py
@@ -0,0 +1,22 @@
+def solution(nickname):
+    answer = ""
+    for letter in nickname:
+        if letter == "l":
+            answer += "I"
+        elif letter == "w":
+            answer += "vv"
+        elif letter == "W":
+            answer += "VV"
+        elif letter == "O":
+            answer += "0"
+        else:
+            answer += letter
+    while len(answer) < 4:
+        answer += "o"
+    if len(answer) > 8:
+        answer = answer[:8]
+    return answer
+
+import sys
+
+print(solution(sys.stdin.readline().strip()))
