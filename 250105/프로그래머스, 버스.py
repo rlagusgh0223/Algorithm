404394247dffde669b4443b474d4b589d2e07440
rlagusgh0223 <hopanman11@naver.com>
2025-01-09 05:42:00 +0900

    2025/01/09

---
+++ 250105/프로그래머스, 버스.py
@@ -0,0 +1,41 @@
+def func1(num):
+    if 0 > num:
+        return 0
+    else:
+        return num
+
+def func2(num):
+    if num > 0:
+        return 0
+    else:
+        return num
+
+def func3(station):
+    num = 0
+    for people in station:
+        if people == "Off":
+            num += 1
+    return num
+
+def func4(station):
+    num = 0
+    for people in station:
+        if people == "On":
+            num += 1
+    return num
+
+def solution(seat, passengers):
+    num_passenger = 0
+    for station in passengers:
+        num_passenger += func4(station)
+        num_passenger -= func3(station)
+    answer = func1(seat-num_passenger)
+
+    return answer
+
+import sys
+
+s = int(sys.stdin.readline())
+passengers = list(sys.stdin.readline().strip().split('"], ["'))
+passengers = [s.split('", "') for s in passengers]
+print(solution(s, passengers))
